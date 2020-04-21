446e52462584866a7e422239632f53ed598b1f6c
dotcirill <dotcirill@gmail.com>
2020-04-21 14:50:14 +0300

    Refactoring

--- src/ui/gamesizing.c
+++ src/ui/gamesizing.c
@@ -11,7 +11,7 @@ void map_xy_to_window_xy(int alpha, int x, int y, float *xw, float *yw) {
     *yw = 1.0 - 2 * y / (float)window_height();
 }
 
-void _set_alpha(Game *game) {
+static void set_alpha(Game *game) {
     // +1 is game margin;
     int a1 = window_height() * (1 - FONT_HEIGHT) / (game->height + 1);
     int a2 = window_width() * (1 - FONT_WIDTH * 30) /
@@ -19,17 +19,17 @@ void _set_alpha(Game *game) {
     game->alpha = a1 < a2 ? a1 : a2;
 }
 
-void _set_px_creature(Game *game) {
+static void set_px_creature(Game *game) {
     game->px_creature = game->alpha * 2 - CELL_CREATURE_PADDING;
 }
 
-void _set_window_xy(Game *g) {
+static void set_window_xy(Game *g) {
     map_xy_to_window_xy(g->alpha, g->width, g->height, &g->window_x,
                         &g->window_y);
 }
 
 void sync_sizing_props(Game *game) {
-    _set_alpha(game);
-    _set_window_xy(game);
-    _set_px_creature(game);
+    set_alpha(game);
+    set_window_xy(game);
+    set_px_creature(game);
 }
